9a80b0463e3f7d6dedfb597de3994f90fa48c161
Jason McCarty <jmccart480@gmail.com>
2022-06-01 20:03:48 -0400

    initial commit

---
+++ Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Employee
+{
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+    public string FullName { get; set; }
+    public string Title { get; set; }
+    public DateTime StartDate { get; set; }
+    private float Salary { get; set; }
+    public int Age { get; set; }
+
+    
+
+    public Employee(string FirstName, string LastName, string Title, DateTime StartDate, int Salary, int Age)
+	{
+		this.FirstName = FirstName;
+		this.LastName = LastName;
+		this.FullName = $"{this.FirstName} {this.LastName}";
+		this.Title = Title;
+		this.StartDate = StartDate;
+		this.Salary = Salary;
+        this.Age = Age;
+    }
+    public void ShowName()
+    {
+        Console.WriteLine($"{this.FullName} and I started on {this.StartDate}");
+    }
+
+    public void DoWork()
+    {
+       Console.WriteLine("I am working");
+    }
+        
+    public void ShowTitle()
+    {
+       Console.WriteLine($"My title is {this.Salary}");
+    }
+    public void ShowPay()
+    {
+       Console.WriteLine($"My pay is { this.Salary}");
+    }
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+class Program
+{ 
+      
+        static void Main(string[] args) 
+        {
+
+        try
+        {
+            Console.WriteLine("Enter in a number.");
+            var t = int.Parse(Console.ReadLine());
+        }
+        catch (Exception)
+        {
+            Console.WriteLine($"An exception was thrown");
+        }
+
+        Employee employee1 = new Employee("Jason", "McCarty", "EHS Manager", DateTime.Now, 15, 30);
+        Employee employee2 = new Employee("Jeremie", "McCarty", "Manager", Convert.ToDateTime("8/20/19"), 45, 30);
+
+           List<Employee> employeeList = new List<Employee>();
+           employeeList.Add(employee1);
+           employeeList.Add(employee2);
+
+            foreach(Employee tempEmployeeObj in employeeList)
+            {
+                tempEmployeeObj.ShowName();
+                tempEmployeeObj.ShowPay();
+                tempEmployeeObj.ShowTitle();
+            }
+   
+    }
+
+        
+        
+        
+        
+        
+        
+}
+
